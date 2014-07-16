b3d5f81253b395e2e4457c106c99c0327e05d0d2
kjwon15 <kjwonmail@gmail.com>
2014-07-17 05:21:22 +0900

    Move js file.

---
+++ karaokeserver/static/master.js
@@ -0,0 +1,42 @@
+function handlerQuery(event) {
+    var target = $(event.target).closest('form');
+    var vendor = target.children('[name=vendor]').val();
+    var queryType = target.children('[name=type]').val();
+    var val = target.children('[name=query]').val();
+    $.ajax({
+        method: target.attr('method'),
+        url: target.attr('action'),
+        data: {
+            'vendor': vendor,
+        queryType: val,
+        },
+    }).success(function(data) {
+        var songs = data.songs;
+        var target = $('#result');
+        var table = $('<table></table>');
+        var header = $(['<tr>',
+            '<th>Vendor</th>',
+            '<th>Number</th>',
+            '<th>Title</th>',
+            '<th>Singer</th>',
+            '</tr>'].join(''));
+
+        target.html(null);
+        table.append(header);
+        for (var i=0; i<songs.length; i++) {
+            var row = $(['<tr>',
+                '<td>' + songs[i].vendor +'</td>',
+                '<td>' + songs[i].number +'</td>',
+                '<td>' + songs[i].title +'</td>',
+                '<td>' + songs[i].singer +'</td>',
+                '</tr>'].join(''));
+            table.append(row);
+        }
+        target.append(table);
+
+    });
+    event.preventDefault();
+}
+$(function() {
+    $('form').submit(handlerQuery);
+});
