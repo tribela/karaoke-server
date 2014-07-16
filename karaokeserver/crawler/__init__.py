d3c25fe810546974ed14adc2e21518e2f7bf3518
kjwon15 <kjwonmail@gmail.com>
2014-07-16 10:34:07 +0900

    crawling from tj.

--- karaokeserver/crawler/__init__.py
+++ karaokeserver/crawler/__init__.py
@@ -1,10 +1,16 @@
-from . import ky
+from . import ky, tj
 from ..database import Song, DbManager
 
 
 def crawl(db_url):
     manager = DbManager(db_url)
+
     vendor_ky = manager.get_vendor('KY')
     songs = (Song(vendor_ky, r['number'], r['title'], r['singer']) for r
              in ky.crawl())
+    manager.add_songs(songs)
+
+    vendor_tj = manager.get_vendor('TJ')
+    songs = (Song(vendor_tj, r['number'], r['title'], r['singer']) for r
+             in tj.crawl())
     manager.add_songs(songs)
