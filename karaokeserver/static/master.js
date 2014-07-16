eeae5e8f463d777315c4da601700bd63ab09643c
kjwon15 <kjwonmail@gmail.com>
2014-07-17 05:23:54 +0900

    send correctly.

--- karaokeserver/static/master.js
+++ karaokeserver/static/master.js
@@ -1,42 +1,44 @@
 function handlerQuery(event) {
-    var target = $(event.target).closest('form');
-    var vendor = target.children('[name=vendor]').val();
-    var queryType = target.children('[name=type]').val();
-    var val = target.children('[name=query]').val();
-    $.ajax({
-        method: target.attr('method'),
-        url: target.attr('action'),
-        data: {
-            'vendor': vendor,
-        queryType: val,
-        },
-    }).success(function(data) {
-        var songs = data.songs;
-        var target = $('#result');
-        var table = $('<table></table>');
-        var header = $(['<tr>',
-            '<th>Vendor</th>',
-            '<th>Number</th>',
-            '<th>Title</th>',
-            '<th>Singer</th>',
-            '</tr>'].join(''));
+  var target = $(event.target).closest('form');
+  var vendor = target.children('[name=vendor]').val();
+  var queryType = target.children('[name=type]').val();
+  var val = target.children('[name=query]').val();
+  var data = {};
 
-        target.html(null);
-        table.append(header);
-        for (var i=0; i<songs.length; i++) {
-            var row = $(['<tr>',
-                '<td>' + songs[i].vendor +'</td>',
-                '<td>' + songs[i].number +'</td>',
-                '<td>' + songs[i].title +'</td>',
-                '<td>' + songs[i].singer +'</td>',
-                '</tr>'].join(''));
-            table.append(row);
-        }
-        target.append(table);
+  data['vendor'] = vendor;
+  data[queryType] = val;
 
-    });
-    event.preventDefault();
+  $.ajax({
+    method: target.attr('method'),
+    url: target.attr('action'),
+    data: data,
+  }).success(function(data) {
+    var songs = data.songs;
+    var target = $('#result');
+    var table = $('<table></table>');
+    var header = $(['<tr>',
+      '<th>Vendor</th>',
+      '<th>Number</th>',
+      '<th>Title</th>',
+      '<th>Singer</th>',
+      '</tr>'].join(''));
+
+    target.html(null);
+    table.append(header);
+    for (var i=0; i<songs.length; i++) {
+      var row = $(['<tr>',
+        '<td>' + songs[i].vendor +'</td>',
+        '<td>' + songs[i].number +'</td>',
+        '<td>' + songs[i].title +'</td>',
+        '<td>' + songs[i].singer +'</td>',
+        '</tr>'].join(''));
+      table.append(row);
+    }
+    target.append(table);
+
+  });
+  event.preventDefault();
 }
 $(function() {
-    $('form').submit(handlerQuery);
+  $('form').submit(handlerQuery);
 });
