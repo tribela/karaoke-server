8cf14bffd87ecb3ee215fd2412edea8363f25a03
kjwon15 <kjwonmail@gmail.com>
2014-07-28 19:11:59 +0900

    Run worker for crawling.

---
+++ worker.py
@@ -0,0 +1,18 @@
+import os
+import schedule
+import time
+from karaokeserver.crawler import crawl
+
+
+DB_URL = os.getenv('DATABASE_URL')
+
+def job():
+    crawl(DB_URL, new=True)
+
+
+if __name__ == '__main__':
+    schedule.every().monday.do(job)
+
+    while 1:
+        schedule.run_pending()
+        time.sleep(10)
